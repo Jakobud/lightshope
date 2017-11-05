c59a372166b2a218bb4994416408d15c02104875
Jake Wilson <jake.e.wilson@gmail.com>
2017-11-05 08:09:41 -0700

    Updated to include pre-req quests as well

--- sql/migrations/20171104225441_world.sql
+++ sql/migrations/20171104225441_world.sql
@@ -9,10 +9,11 @@ INSERT INTO `migrations` VALUES ('20171104225441');
 -- Add your query below.
 
 --This update brings the following quests minimum level inline with eachother according to:
+-- http://web.archive.org/web/20050316083722/http://wow.allakhazam.com:80/db/quest.html?wquest=3561
 -- http://web.archive.org/web/20050117052220/http://wow.allakhazam.com:80/db/quest.html?wquest=3565
 
 -- Update quest minimum levels
-UPDATE quest_template SET minLevel = 48 WHERE entry IN (3562,3563,3564,3565);
+UPDATE quest_template SET minLevel = 48 WHERE entry IN (3518, 3541, 3542, 3561, 3562, 3563, 3564, 3565);
 
 -- End of migration.
 END IF;
